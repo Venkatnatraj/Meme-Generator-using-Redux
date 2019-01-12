13fa83b57a81dae13817c5a9bd5d65721165e943
Venkatnatraj <t.venkatanatraj95@gmail.com>
2019-01-12 17:32:10 +0530

    Actions passed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import {createStore, applyMiddleware} from 'redux';
-// import {Provider} from 'react-redux';
+import {Provider} from 'react-redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
